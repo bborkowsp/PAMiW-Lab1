a65330f92b5a00e876f16d4205f4c49c9b727b18
bborkowsp <pawel.borkowski0704@gmail.com>
2023-11-17 22:58:08 +0100

    L6

---
+++ L6/P05Shop.API/Models/DataContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P06Shop.Shared.VehicleDealership;
+using P07Shop.DataSeeder;
+
+namespace P05Shop.API.Models
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // fluent api 
+            // modelBuilder.Entity<Vehicle>()
+            //     .Property(p => p.Barcode)
+            //     .IsRequired()
+            //     .HasMaxLength(12);
+
+            modelBuilder.Entity<Vehicle>()
+                .Property(p => p.Model)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Vehicle>()
+                .Property(p => p.Fuel)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            // modelBuilder.Entity<Vehicle>()
+            //  .Property(p => p.Price)
+            //  .HasColumnType("decimal(8,2)");
+
+            // data seed 
+
+            modelBuilder.Entity<Vehicle>().HasData(VehicleSeeder.GenerateVehicleData());
+        }
+    }
+}
+// instalacja dotnet ef 
+//dotnet tool install --global dotnet-ef
+
+// aktualizacja 
+//dotnet tool update --global dotnet-ef
+
+// dotnet ef migrations add [nazwa_migracji]
+// dotnet ef database update 
+
+
+// cofniecie migraji niezaplikowanych 
+//dotnet ef migrations remove
---
+++ L6/P07Shop.DataSeeder/VehicleSeeder.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using P06Shop.Shared.VehicleDealership;
+
+namespace P07Shop.DataSeeder
+{
+    public class VehicleSeeder
+    {
+        public static List<Vehicle> GenerateVehicleData()
+        {
+            int productId = 1;
+            var productFaker = new Faker<Vehicle>()
+                .UseSeed(123456)
+                .RuleFor(x => x.Model, x => x.Vehicle.Model())
+                .RuleFor(x => x.Fuel, x => x.Vehicle.Fuel())
+                .RuleFor(x => x.Id, x => productId++);
+
+            return productFaker.Generate(50).ToList();
+        }
+    }
+}
