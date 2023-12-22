f8a8aeb063211969600ecba38b537114a441bcf2
bborkowsp <pawel.borkowski0704@gmail.com>
2023-12-22 19:45:30 +0100

    main page theme

--- Projekt/P12MAUI.Client/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/P12MAUI.Client.AssemblyInfo.cs
+++ Projekt/P12MAUI.Client/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/P12MAUI.Client.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P12MAUI.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+81b24099f60f0184904af262a9c0c9940ef9b55e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7deb7120905b3fb8bbbd2a11f7f0661dc0b0f2ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("P12MAUI.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P12MAUI.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
