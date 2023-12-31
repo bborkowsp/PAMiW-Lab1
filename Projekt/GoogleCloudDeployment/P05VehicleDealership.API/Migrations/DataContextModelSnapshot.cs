c87b4b5c6da153a1df5111f0cd55e59692340d8f
bborkowsp <pawel.borkowski0704@gmail.com>
2023-12-31 12:43:12 +0100

    cloud hosting

--- Projekt/GoogleCloudDeployment/P05VehicleDealership.API/Migrations/DataContextModelSnapshot.cs
+++ Projekt/GoogleCloudDeployment/P05VehicleDealership.API/Migrations/DataContextModelSnapshot.cs
@@ -17,10 +17,10 @@ namespace P05VehicleDealership.API.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "6.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
 
             modelBuilder.Entity("P05VehicleDealership.API.Auth.User", b =>
                 {
@@ -28,7 +28,7 @@ namespace P05VehicleDealership.API.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
                     b.Property<DateTime>("DateCreated")
                         .HasColumnType("datetime2");
@@ -64,7 +64,7 @@ namespace P05VehicleDealership.API.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
                     b.Property<string>("Fuel")
                         .IsRequired()
