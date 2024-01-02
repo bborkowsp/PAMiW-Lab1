1fb3e5a1fbc91322718f1c6fcf7b00a09170362b
bborkowsp <pawel.borkowski0704@gmail.com>
2024-01-02 14:30:50 +0100

    colors updates

--- Projekt/P12MAUI.Client/obj/Debug/net7.0-android/P12MAUI.Client.AssemblyInfo.cs
+++ Projekt/P12MAUI.Client/obj/Debug/net7.0-android/P12MAUI.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P12MAUI.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7226fcce88abf10ad41eeab8c753c188d94bb8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+056aea13a2b1410ee231181e0bf44e60cd78eb27")]
 [assembly: System.Reflection.AssemblyProductAttribute("P12MAUI.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P12MAUI.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
